244d7021eb9141c247db42822488cc5e1661e1fc
hraheem01-ux <hraheem01@gmail.com>
2026-03-27 01:04:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { Resend } from "resend";
-import fetch from "node-fetch";
+
 
 const app = express();
 app.use(cors());
